0281f06e1210122250427df929b2e60d984c58a9
Olli <olli.mustakallio@aalto.fi>
2018-12-12 16:18:36 +0200

    Replaced random templates with an extedable system containing links with url tags.

--- store/views.py
+++ store/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'base.html', {'context': 'context'})
+    return render(request, 'index.html', {'context': 'context'})
+
+
+def login(request):
+    return render(request, 'login.html', {'context': 'context'})
