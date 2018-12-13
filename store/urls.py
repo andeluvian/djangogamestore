d051790b88bbf147c99c2939c314c0174ef20e86
Otso Teperi <otso.teperi@gmail.com>
2018-12-13 11:17:21 +0200

    Add URLconf

---
+++ store/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'), # frontpage
+
+    path('login/', views.login, name='login'), # login
+    path('logout/', views.index, name='index'), # logout
+    path('register/', views.index, name='index'), # register player or developer
+
+    path('player/', views.index, name='index'), # show bought games @player
+    path('player/purchases/', views.index, name='index'), # show purchase history @player
+
+    path('developer/', views.index, name='index'), # show own games @developer
+    path('developer/submit/', views.index, name='index'), # submit new game @developer
+
+    path('search/', views.index, name='index'), # search, parameters via request.GET
+
+    path('game/<slug:title>', views.index, name='index'), # game details
+    path('game/<slug:title>/buy/', views.index, name='index'), # buy game @player
+    path('game/<slug:title>/play/', views.index, name='index'), # play game @player @bought
+    path('game/<slug:title>/edit/', views.index, name='index'), # edit game @developer
+    path('game/<slug:title>/delete/', views.index, name='index'), # delete game @developer
+]
