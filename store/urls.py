2e2a634c01e16a1f12c3fcb0c73917cd7fb2801a
Olli <olli.mustakallio@aalto.fi>
2018-12-29 13:12:51 +0200

    Corrected name arguments in path function

--- store/urls.py
+++ store/urls.py
@@ -1,24 +1,25 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), # frontpage
+    path('', views.index, name='index'),  # front page
 
-    path('login/', views.login, name='login'), # login
-    path('logout/', views.index, name='index'), # logout
-    path('register/', views.index, name='index'), # register player or developer
+    path('login/', views.login, name='login'),  # login
+    path('logout/', views.index, name='logout'),  # logout
+    path('register/', views.index, name='register'),  # register player or developer
 
-    path('player/', views.index, name='index'), # show bought games @player
-    path('player/purchases/', views.index, name='index'), # show purchase history @player
+    path('player/', views.index, name='player_home'),  # show bought games @player
+    path('player/purchases/', views.index, name='purchases'),  # show purchase history @player
 
-    path('developer/', views.index, name='index'), # show own games @developer
-    path('developer/submit/', views.index, name='index'), # submit new game @developer
+    path('developer/', views.index, name='developer_home'),  # show own games @developer
+    path('developer/submit/', views.index, name='submit'),  # submit new game @developer
 
-    path('search/', views.index, name='index'), # search, parameters via request.GET
+    path('search/', views.index, name='search'),  # search, parameters via request.GET
 
-    path('game/<slug:title>', views.index, name='index'), # game details
-    path('game/<slug:title>/buy/', views.index, name='index'), # buy game @player
-    path('game/<slug:title>/play/', views.index, name='index'), # play game @player @bought
-    path('game/<slug:title>/edit/', views.index, name='index'), # edit game @developer
-    path('game/<slug:title>/delete/', views.index, name='index'), # delete game @developer
+    path('game/<slug:title>', views.index, name='game'),  # game details
+    path('game/<slug:title>/buy/', views.index, name='buy'),  # buy game @player
+    path('game/<slug:title>/play/', views.index, name='play'),  # play game @player @bought
+    path('game/<slug:title>/edit/', views.index, name='edit'),  # edit game @developer
+    path('game/<slug:title>/delete/', views.index, name='delete'),  # delete game @developer
 ]
