6a255bcc80efd80d3223a4db952d273191aca1c6
Olli Mustakallio <olli.mustakallio@aalto.fi>
2019-01-03 20:55:40 +0200

    Added user count context

--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {'context': 'context'})
+    user_count = User.objects.count()  # Only to verify the registration form works, delete later
+    return render(request, 'index.html', {'user_count': user_count})
 
 
 def login(request):
