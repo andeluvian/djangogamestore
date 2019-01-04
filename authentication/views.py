d33f2a90dedc85e8c667ef644334551ae5730298
Olli Mustakallio <olli.mustakallio@aalto.fi>
2019-01-04 14:55:55 +0200

    Minor indentation fixes

--- authentication/views.py
+++ authentication/views.py
@@ -1,70 +1,42 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User, Group
-from django.contrib.auth import (
-    authenticate,
-    get_user_model,
-    login,
-    logout
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
 
-)
-
-
-##This is old login form, test do not work
-""""
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created succesfully! Login and enjoy our site!')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'register.html', {'form':form})
-"""
-
-from .forms import (
-UserLoginForm,
-UserRegistrationForm
-)
+from .forms import UserLoginForm, UserRegistrationForm
 
 
 def login_view(request):
-#    next = request.Get.get('next')
+    #    next = request.Get.get('next')
     form = UserLoginForm(request.POST or None)
     if form.is_valid():
-         username = form.cleaned_data.get('username')
-         password = form.cleaned_data.get('password')
-         user = authenticate(username=username, password=password)
-         login(request, user)
-         if next:
-             return redirect(next)
-         return redirect('/')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+
     context = {
         'form': form
     }
-    print('Hello')
-    return render(request, 'login.html', context)
+    return render(request, 'registration/login.html', context)
+
 
 def register_view(request):
-#     next = request.Get.get('next')
-     form = UserRegistrationForm(request.POST or None)
-     if form.is_valid():
-         user = form.save(commit=False)
-         password = form.cleaned_data.get('password')
-         user.set_password(password)
-         user.save()
-         new_user = authenticate(username=user.username, password=password)
-         login(request, new_user)
-         if next:
-             return redirect(next)
-         return redirect('/')
-     context = {
-         'form': form
+    #     next = request.Get.get('next')
+    form = UserRegistrationForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        if next:
+            return redirect(next)
+        return redirect('/')
 
-     }
-
-     return render(request, "register.html", context)
+    context = {
+        'form': form
+    }
+    return render(request, "register.html", context)
