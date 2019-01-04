6e8e65839f96fdf5bd8a860b37fdd3b5873a2a91
Olli Mustakallio <olli.mustakallio@aalto.fi>
2019-01-04 14:55:27 +0200

    Minor indentation fixes

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,9 +1,5 @@
 from django import forms
-from django.contrib.auth import (
-    authenticate,
-    get_user_model
-
-)
+from django.contrib.auth import authenticate, get_user_model
 
 User = get_user_model()
 
@@ -13,18 +9,18 @@ class UserLoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
     def clean(self, *args, **kwargs):
-         username = self.cleaned_data.get('username')
-         password = self.cleaned_data.get('password')
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
 
-         if username and password:
-             user = authenticate(username=username, password=password)
-         if not user:
-             raise forms.ValidationError('This user does not exist')
-         if not user.check_password(password):
-             raise forms.ValidationError('Password missmatch, please check password')
-         if not user.is_active:
-             raise forms.ValidationError('This user is not active')
-         return super(UserLoginForm, self).clean(*args, **kwargs)
+        if username and password:
+            user = authenticate(username=username, password=password)
+        if not user:
+            raise forms.ValidationError('This user does not exist')
+        if not user.check_password(password):
+            raise forms.ValidationError('Password mismatch, please check password')
+        if not user.is_active:
+            raise forms.ValidationError('This user is not active')
+        return super(UserLoginForm, self).clean(*args, **kwargs)
 
 
 class UserRegistrationForm(forms.ModelForm):
@@ -45,7 +41,8 @@ class UserRegistrationForm(forms.ModelForm):
         email = self.cleaned_data.get('email')
         email2 = self.cleaned_data.get('email2')
         if email != email2:
-            raise forms.ValidationError("emails do not match")
+            raise forms.ValidationError("Emails do not match")
+
         email_qs = User.objects.filter(email=email)
         if email_qs.exists():
             raise forms.ValidationError(
