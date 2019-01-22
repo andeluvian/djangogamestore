4c817e3d99c505491d3d8e61515069d8afef3771
Otso Teperi <otso.teperi@gmail.com>
2019-01-22 21:29:49 +0200

    Add price formatting as template tag

---
+++ payment/templatetags/payment_extras.py
@@ -0,0 +1,10 @@
+import locale
+from django import template
+
+
+register = template.Library()
+locale.setlocale(locale.LC_ALL, 'fi_FI.UTF-8')
+
+@register.filter(name='price')
+def price(value):
+    return locale.currency(value, grouping=True)
