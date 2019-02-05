1d253f4e36c0a7058e34b2fcc2330a157b49deb0
Otso Teperi <otso.teperi@gmail.com>
2019-02-05 22:07:55 +0200

    Fix currency formatting

--- payment/templatetags/payment_extras.py
+++ payment/templatetags/payment_extras.py
@@ -1,15 +1,13 @@
-import locale
 from django import template
 from payment.models import Transaction
 
 
 register = template.Library()
-locale.setlocale(locale.LC_ALL, 'fi_FI.UTF-8')
 
 
 @register.filter(name='price')
 def price(value):
-    return locale.currency(value, grouping=True)
+    return '%.2f €' % value
 
 
 @register.filter(name='has_game')
