913e6a685173b4d5c977316ddb3c911cb392a8bd
andeluvian <kenneth.forsman@gmail.com>
2019-02-12 19:44:29 +0200

    merge with master

--- payment/templatetags/payment_extras.py
+++ payment/templatetags/payment_extras.py
@@ -3,12 +3,11 @@ from payment.models import Transaction
 
 
 register = template.Library()
-<<<<<<< HEAD
 ##locale.setlocale(locale.LC_ALL, 'fi_FI.UTF-8')
 @register.filter(name='price')
 def price(value):
     return locale.currency(value, grouping=True)
-=======
+
 
 
 @register.filter(name='price')
@@ -24,4 +23,3 @@ def price(value):
 @register.filter(name='has_game')
 def has_game(user, game):
     return Transaction.objects.filter(user=user).filter(game=game).filter(state='SUCCESS').exists()
->>>>>>> 28c5ff06e29549a46483ec01fa39a403b5d9f437
