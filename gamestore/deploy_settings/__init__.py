236defab90f48127464c05e0a7e18a5bdfaf5e7f
Olli Mustakallio <olli.mustakallio@aalto.fi>
2019-02-19 01:45:53 +0700

    Config S3

--- gamestore/deploy_settings/__init__.py
+++ gamestore/deploy_settings/__init__.py
@@ -31,4 +31,4 @@ AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
 MEDIA_URL = 'http://%s.s3.amazonaws.com/s3/buckets/' % AWS_STORAGE_BUCKET_NAME
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
