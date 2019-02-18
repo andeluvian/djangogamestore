59be49453c5a86ea4fe8d5777d8b79a233261f1f
Olli Mustakallio <olli.mustakallio@aalto.fi>
2019-02-19 01:30:53 +0700

    Add AWS S3

--- gamestore/deploy_settings/__init__.py
+++ gamestore/deploy_settings/__init__.py
@@ -11,6 +11,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '.herokuapp.com',
 ]
+INSTALLED_APPS += ('storages',)
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
@@ -22,3 +23,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECURE_SSL_REDIRECT = False
 
 SITE_ID = 2
+
+# AWS S3 Settings
+AWS_QUERYSTRING_AUTH = False
+AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
+MEDIA_URL = 'http://%s.s3.amazonaws.com/s3/buckets/' % AWS_STORAGE_BUCKET_NAME
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
