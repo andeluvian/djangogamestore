9eb8af95bc4295841d6a7feb379a4c38459dc002
Olli Mustakallio <olli.mustakallio@aalto.fi>
2019-02-18 19:17:55 +0700

    Add heroku to allowedhosts

--- gamestore/deploy_settings/__init__.py
+++ gamestore/deploy_settings/__init__.py
@@ -3,4 +3,9 @@ from gamestore.settings import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+ALLOWED_HOSTS = [
+    'localhost',
+    '.herokuapp.com',
+]
+
 # source: https://teamtreehouse.com/library/making-our-project-productionready
