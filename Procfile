3425f089c01b3dd418e80339b14a982d2520cb48
Olli Mustakallio <olli.mustakallio@aalto.fi>
2019-02-18 19:12:54 +0700

    Add production settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath gamestore gamestore.deploy --log-file -
---
+++ gamestore/deploy_settings/__init__.py
@@ -0,0 +1,6 @@
+from gamestore.settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+# source: https://teamtreehouse.com/library/making-our-project-productionready
